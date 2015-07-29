bddf201333faa976aac827d3b7fc1babe16f1c45
Ian Kronquist <iankronquist@teleport.com>
2015-07-28 22:15:10 -0700

    These don't work but are a start

---
+++ Group3/fixtures.sql
@@ -0,0 +1,5 @@
+insert into p2_users values (0, 'john', 'badassasaservice', 'john', 'smith', 42, 'M', 32, 33, 'here', '1999-12-01', '1999-12-01', '1999-12-01', '1999-12-01', 1, 2, 3, 4);
+
+insert into p2_users values (1, 'jane', 'fancyusername', 'jane', 'doe', 24, 'F', 23, 33, 'here', '1999-12-01', '1999-12-01', '1999-12-01', '1999-12-01', 4, 3, 2, 1);
+
+insert into donations values (0, 0, '1999-12-01', 'here', 'O-', 'whole blood', 2, False, '1999-12-01');
