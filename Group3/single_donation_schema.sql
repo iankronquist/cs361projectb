7b7ec1879dfc2877aef549c64cf7394c0513b48b
Ian Kronquist <iankronquist@teleport.com>
2015-07-28 22:13:48 -0700

    Don't need many to many table

--- Group3/single_donation_schema.sql
+++ Group3/single_donation_schema.sql
@@ -8,18 +8,10 @@ CREATE TABLE donations (
     blood_type varchar(255),
     donation_type varchar(255),
     amount int,
+    used bool,
+    date_used date,
 
     UNIQUE (id),
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES p2_users(id)
 )ENGINE=InnoDB;
-
-CREATE TABLE users_to_donations (
-    donation_id int NOT NULL,
-    user_id int NOT NULL,
-
-    FOREIGN KEY (user_id) REFERENCES p2_users(id),
-    FOREIGN KEY (donation_id) REFERENCES donations(id)
-)ENGINE=InnoDB;
-
-
