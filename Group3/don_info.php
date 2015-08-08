764d809941e6bc99605c8219ba7259dabac92415
Moseco <hampus_hammarlund@hotmail.com>
2015-08-08 14:34:06 -0700

    Create don_info.php
    I will add some more later today. But its something to put into master site for now

---
+++ Group3/don_info.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+	<h1> Donation eligibility</h1>
+	<h2> Medications and vaccinations</h2>
+	
+	<h3> Antibiotics </h3> 
+	<p> A donor with an acute infection should not donate. The reason for antibiotic use must be evaluated to determine if the donor has a bacterial infection that could be transmissible by blood.
+Acceptable after finishing oral antibiotics for an infection (bacterial or viral). May have taken last pill on the date of donation. Antibiotic by injection for an infection acceptable 10 days after last injection. Acceptable if you are taking antibiotics to prevent an infection, for example, following dental procedures or for acne. Some conditions which require antibiotics to prevent an infection must still be evaluated at the time of donation by the responsible medical director. If you have a temperature above 99.5 F, you may not donate.</p>
+	
+	<h3>Birth Control</h3>
+	<p>Women on oral contraceptives or using other forms of birth control are eligible to donate.</p>
+
+	<h3>Immunization, Vaccination</h3>
+
+	<pre>
+	Acceptable if you were vaccinated for influenza, tetanus or meningitis, providing you are symptom-free and fever-free. Includes the Tdap vaccine.
+	Acceptable if you received an HPV Vaccine (example, Gardasil).
+	Wait 4 weeks after immunizations for German Measles (Rubella), MMR (Measles, Mumps and Rubella), Chicken Pox and Shingles.
+	Wait 2 weeks after immunizations for Red Measles (Rubeola), Mumps, Polio (by mouth), and Yellow Fever vaccine.
+	Wait 21 days after immunization for hepatitis B as long as you are not given the immunization for exposure to hepatitis B.
+	<b>Smallpox vaccination and did not develop complications</b>
+	Wait 8 weeks (56 days) from the date of having a smallpox vaccination as long as you have had no complications. 
+	Complications may include skin reactions beyond the vaccination site or general illness related to the vaccination.
+	<b>Smallpox vaccination and developed complications</b>
+	Wait 14 days after all vaccine complications have resolved or 8 weeks (56 days) from the date of having had the smallpox vaccination whichever is the longer period of time. 
+	You should discuss your particular situation with the health historian at the time of donation. 
+	Complications may include skin reactions beyond the vaccination site or general illness related to the vaccination.
+	<b>Smallpox vaccination – close contact with someone who has had the smallpox vaccine in the last eight weeks and you did not develop any skin lesions or other symptoms.</b>
+	Eligible to donate.
+	<b>Smallpox vaccination – close contact with someone who has had the vaccine in the last eight weeks and you have since developed skin lesions or symptoms.</b>
+	Wait 8 weeks (56 days) from the date of the first skin lesion or sore. 
+	You should discuss your particular situation with the health historian at the time of donation. 
+	Complications may include skin reactions or general illness related to the exposure.</pre>
+
+	<h3>General Health Considerations</h3>
+	<pre>
+	<b>Allergy, Stuffy Nose, Itchy Eyes, Dry Cough</b>
+	Acceptable as long as you feel well, have no fever, and have no problems breathing through your mouth.
+	<b>Cold, Flu</b>
+	Wait if you have a fever or a productive cough (bringing up phlegm)
+	Wait if you do not feel well on the day of donation.
+	Wait until you have completed antibiotic treatment for sinus, throat or lung infection.	
+	<b>Donation Intervals</b>
+	Wait at least 8 weeks between whole blood (standard) donations.
+	Wait at least 7 days between platelet (pheresis) donations.
+	Wait at least 16 weeks between double red cell (automated) donations.	
+	<b>Weight/Height</b>
+	You must weigh at least 110 lbs to be eligible for blood donation for your own safety.
+	Students who donate at high school drives and donors 18 years of age or younger must also meet additional height and weight 
+	requirements for whole blood donation 
+	(applies to girls shorter than 5'6" and boys shorter than 5').
+	Blood volume is determined by body weight and height. Individuals with low blood volumes may not tolerate the removal of the required 
+	volume of blood given with whole blood donation. 
+	There is no upper weight limit as long as your weight is not higher than the weight limit of the donor bed/lounge you are using. 
+	You can discuss any upper weight limitations of beds and lounges with your local health historian.
+	<b>Unable to Give Blood?</b>
+	You can help people facing emergencies by making a financial donation to support the Red Cross’s greatest needs. 
+	Your gift enables the Red Cross to ensure an ongoing blood supply, provide humanitarian support to families in need and prepare communities by teaching lifesaving skills. 
+	Make a financial gift today.</pre>
+
+</html>
