b015352cb4d2b974c8daafe69cbef3e4084f64f4
barlanj <barlanj@onid.oregonstate.edu>
2015-08-07 23:59:19 -0400

    fixed link for Supply Info

--- Group1/navbar.php
+++ Group1/navbar.php
@@ -5,7 +5,7 @@
         <a href="../Group1/logout.php">LOGOUT</a>| 
         <a href="../Group2/priority2.php">DONATION ELIGIBILITY</a> | 
         <a href="../Group1/userProfile.php">PROFILE PAGE</a> | 
-        <a href="../Group3/checkBloodSupply.php">SUPPLY INFO</a>
+        <a href="../Group1/checkBloodSupply.php">SUPPLY INFO</a>
     </div>
     <p class="navbar-text navbar-right">Logged in as <a href="#" class="navbar-link"><?php echo "$username"; ?></a></p>
   </div>
